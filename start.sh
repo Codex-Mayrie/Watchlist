d0ebfbf97f30249318cdc17d638006ebc619482f
shee1998 <marywanjikumburu1@gmail.com>
2019-11-22 11:41:44 +0300

    finish the application

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='41f2a4ea837a77252dcaf3541ccc5370'
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+export MAIL_USERNAME=<Your Email Address>
+export MAIL_PASSWORD=<Your Email Password>
+python3.6 manage.py server
